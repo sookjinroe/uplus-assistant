723eff3226c136698414ee88c72c0852acd2ff37
sookjinroe <sookjinroe@gmail.com>
2025-06-25 10:57:21 +0900

    Updated package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  envDir: './',
-  envPrefix: 'VITE_',
-  define: {
-    __DEV__: mode === 'development',
-  },
-}));
+});
