c219fd358cc30b3de5e1f9f31ad730d98ba100da
sookjinroe <sookjinroe@gmail.com>
2025-06-25 10:56:27 +0900

    Supabase 환경 분리 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+  envDir: './',
+  envPrefix: 'VITE_',
+  define: {
+    __DEV__: mode === 'development',
+  },
+}));
